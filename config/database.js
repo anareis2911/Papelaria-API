d163705fa8ed496e76b714f77b18f74282e8c3ce
anareis2911 <acr291104@gmail.com>
2024-01-18 14:01:46 -0300

    Configuração do Banco de Dados

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const dbName = 'Papelaria';
+const dbUser = 'Estoquista';
+const dbPassword = 'estoquista2024';
+const dbHost = 'localhost';
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: 'mysql'
+})
+
+module.exports = sequelize
