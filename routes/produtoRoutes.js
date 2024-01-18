73c9fdc34e854eff391a1b7d46bc073aa6ef77dd
anareis2911 <acr291104@gmail.com>
2024-01-18 14:52:06 -0300

    Criação do Controller Produto

---
+++ routes/produtoRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const ProdutoController = require('../controllers/ProdutoController');
+
+router.post('/produtos', ProdutoController.createProduto);
