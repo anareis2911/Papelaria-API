5de075b60641fa4dd3baf4a764c3673d30ca65c2
anareis2911 <acr291104@gmail.com>
2024-01-19 14:40:11 -0300

    test: finalização do teste das rotas do produto

--- controllers/ProdutoController.js
+++ controllers/ProdutoController.js
@@ -15,4 +15,16 @@ const getProdutoById = async (req, res) => {
     const produto = await Produto.findByPk(req.params.id)
     res.json(produto)
 }
-module.exports = {createProduto,getAllProdutos,getProdutoById}
+
+const updateProduto = async (req, res) => {
+    const produto = await Produto.findByPk(req.params.id)
+    await produto.update(req.body);
+    res.json({message: 'Produto atualizado com sucesso!'});
+}
+
+const deleteProduto = async (req, res) => {
+    const produto = await Produto.findByPk(req.params.id)
+    await produto.destroy();
+    res.json({message: 'Produto deletado com sucesso'});
+}
+module.exports = {createProduto, getAllProdutos, getProdutoById, updateProduto, deleteProduto}
--- routes/produtoRoutes.js
+++ routes/produtoRoutes.js
@@ -5,5 +5,7 @@ const ProdutoController = require('../controllers/ProdutoController');
 router.post('/produtos', ProdutoController.createProduto);
 router.get('/produtos', ProdutoController.getAllProdutos);
 router.get('/produtos/:id', ProdutoController.getProdutoById);
+router.put('/produtos/:id', ProdutoController.updateProduto);
+router.delete('/produtos/:id', ProdutoController.deleteProduto);
 
 module.exports = router
